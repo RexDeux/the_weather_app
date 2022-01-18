de0e0ba9293e322d41e9ba56b4f5c3f0cb493133
RexDeux <nm.cdc@hotmail.com>
2022-01-18 17:48:15 +0000

    fixed the views.py

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -9,26 +9,32 @@ from .forms import CityForm
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=a2290f5132b80143df242aa1fe7a093d'
 
-    city = 'Braga'
-    city_weather = requests.get(url.format(city)).json()
+    #city = 'Braga'
+    #city_weather = requests.get(url.format(city)).json()
     cities = City.objects.all()
+    
     weather_data = []
-    print(city_weather)
+    #print(city_weather)
 
     if request.method == 'POST' :
         form = CityForm(request.POST)
         form.save()
+
     form = CityForm()
 
     for city in cities:
-        city_weather = requests.get(url.format(city)).json()
-        weather = {
+
+        a = requests.get(url.format(city)).json()
+
+        city_weather = {
             'city' : city,
-            'temperature' : city_weather['main']['temp'],
+            'temperature' : a['main']['temp'],
             #'coord' : city_weather['lat']['lon'],
-            'description' : city_weather['weather'][0]['description'],
-            'icon' : city_weather['weather'][0]['icon']
+            'description' : a['weather'][0]['description'],
+            'icon' : a['weather'][0]['icon']
         }
+
+        weather_data.append(city_weather)
 
     context = {'weather_data' : weather_data, 'form' : form}
     return render(request, 'weather/index.html', context)
